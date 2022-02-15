3e04e2462df0c19001ea4ff409f938cac527cf15
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2022-02-15 17:14:58 +0100

    Modified Fighter seeder

--- database/seeders/FighterSeeder.php
+++ database/seeders/FighterSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Fighter;
 use Illuminate\Database\Seeder;
 
 class FighterSeeder extends Seeder
@@ -13,6 +14,36 @@ class FighterSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Fighter::factory(10)->create();
+        Fighter::create([
+            'name' => "Victor",
+            'type' => "",
+            'details' => "You give up your ability to attack for buffing your on-field cards with 20% attack and hp.",
+            'level' => 1,
+            'hp' => 1000,
+            'attack' => 0]);
+
+        Fighter::create([
+            'name' => "",
+            'type' => "",
+            'details' => "For 2 turns, you gain 15% extra attack by expending 15% of your current health. It's cooldown is 2 turns.",
+            'level' => 1,
+            'hp' => 1000,
+            'attack' => 100]);
+
+        Fighter::create([
+            'name' => "Victor",
+            'type' => "",
+            'details' => "Your card's stats are decreased by 15%, but they also cost 20% less.",
+            'level' => 1,
+            'hp' => 1000,
+            'attack' => 100]);
+
+        Fighter::create([
+            'name' => "Victor",
+            'type' => "",
+            'details' => "Whenever you're under max hp, you gain 5% of max hp back every turn.",
+            'level' => 1,
+            'hp' => 1000,
+            'attack' => 95]);
     }
 }
