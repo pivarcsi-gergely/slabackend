b6e7f6833a81c4435b2402ac760d0383913513b0
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2022-02-15 22:59:58 +0100

    Finalized CardSeeder

--- database/seeders/CardSeeder.php
+++ database/seeders/CardSeeder.php
@@ -19,65 +19,72 @@ class CardSeeder extends Seeder
             'type' => "Assassin",
             'details' => "Automatically dogdes an attack when summoned, and once every 4 turns.",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 50,
-            'summon_cost' => 10,]);
+            'hp' => 250,
+            'attack' => 450,
+            'summon_cost' => 600,
+        ]);
         Card::create([
             'name' => "Francis",
             'type' => "Tank",
             'details' => "Decreases incoming damage by 30% if it is on the front lane.",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 50,
-            'summon_cost' => 10,]);
+            'hp' => 1000,
+            'attack' => 150,
+            'summon_cost' => 600,
+        ]);
         Card::create([
             'name' => "Alejandro",
             'type' => "Soldier",
             'details' => "For every soldier type of card onfield, boosts attack and hp by 15%. Max 4 stacks.",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 50,
-            'summon_cost' => 10,]);
+            'hp' => 400,
+            'attack' => 200,
+            'summon_cost' => 450,
+        ]);
         Card::create([
             'name' => "Cecilia",
             'type' => "Mage",
-            'details' => "Pushes on card to the other lane and can attack after once every 2 turns.",
+            'details' => "Once every 2 turns, push a card that you attack back a lane, then attack it.",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 50,
-            'summon_cost' => 10,]);
+            'hp' => 150,
+            'attack' => 350,
+            'summon_cost' => 400,
+        ]);
         Card::create([
             'name' => "Footman",
             'type' => "Support",
             'details' => "Only card that can operate the building type cards",
             'level' => 1,
-            'hp' => 10,
+            'hp' => 100,
             'attack' => 0,
-            'summon_cost' => 1,]);
+            'summon_cost' => 100,
+        ]);
         Card::create([
             'name' => "Chloe",
             'type' => "Support",
-            'details' => "Heals onfield cards by 20% of it's own hp",
+            'details' => "Heals onfield cards by 20% of it's own max hp, every turn.",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 0,
-            'summon_cost' => 1,]);
+            'hp' => 550,
+            'attack' => 50,
+            'summon_cost' => 350,
+        ]);
         Card::create([
             'name' => "Catapult",
             'type' => "Building",
-            'details' => "Damaging enemy cards based on the number of people operating it.",
+            'details' => "Damaging enemy cards based on the number of people operating it (max 3).",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 50,
-            'summon_cost' => 10,]);
+            'hp' => 150,
+            'attack' => 100,
+            'summon_cost' => 200,
+        ]);
         Card::create([
-            'name' => "Poisoned food",
+            'name' => "Mad Dog",
             'type' => "Taunt",
-            'details' => "Decreases attack by 20% for 2 turns.",
+            'details' => "Decreases the attacking card's attack by 20% for 2 turns.",
             'level' => 1,
-            'hp' => 100,
-            'attack' => 50,
-            'summon_cost' => 10,]);
-                        
+            'hp' => 200,
+            'attack' => 100,
+            'summon_cost' => 200,
+        ]);
     }
 }
