cb7322bfd2e717ef51e00a094ca71de6aee21013
yournamem <email@example.com>
2022-03-02 13:13:13 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\CardController;
-use App\Http\Controllers\FighterController;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('/cards', CardController::class);
-Route::apiResource('/fighters', FighterController::class);
-Route::apiResource('/users', UserController::class);
