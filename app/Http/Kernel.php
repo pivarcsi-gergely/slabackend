47c76ea2326a983b91c84af1fcce787d621f658d
TotpalIstvan <totpal.istvan@petrik.hu>
2022-04-19 11:26:23 +0200

    cors hozzáadása

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,9 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'cors' => [
+            \Barryvdh\Cors\HandleCors::class,
+        ],
     ];
 
     /**
